95430a2c5b49626a44f003bbeb80d1b069f68d58
JulienQC <julien.cusset33@gmail.com>
2018-03-05 23:22:17 -0500

    Décentralisation des noteset affectation des contrôles de chaque note

---
+++ Assets/Scripts/NoteController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class NoteController : MonoBehaviour{
+
+    public GameObject projectilePrefab;
+    public KeyCode key;
+    public float forceFactor;
+
+    private GameObject prefab;
+    private GameObject player;
+    private PlayerMovement mover;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        mover = player.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            Vector3 force = transform.position - player.transform.position;
+            mover.Attract(force * forceFactor);
+        }
+    }
+
+    public void SetKey(KeyCode k)
+    {
+        key = k;
+    }
+
+    public void SendProjectile(float angle, float ray, float speed)
+    {
+        Vector3 position = new Vector3(ray * Mathf.Cos(angle), 1, ray * Mathf.Sin(angle));
+        Quaternion rotation = Quaternion.Euler(90, 0, Mathf.Rad2Deg * angle - 90);
+        Vector3 target = new Vector3(0, 1, 0);
+        ProjectileController proj = ProjectileController.Create(projectilePrefab, position, rotation, speed, target);
+    }
+}
--- Assets/Scripts/NotesController.cs
+++ Assets/Scripts/NotesController.cs
@@ -3,28 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NotesController : MonoBehaviour {
-
-    public GameObject notePrefab;
-    public GameObject projectilePrefab;
+    
     public float ray = 10.0f;
     public float projectileSpeed = 10f;
-    public List<KeyCode> keys;
     public float timeInterval = 2;
 
+    private NoteController[] children;
     private float time;
 
 	// Use this for initialization
 	void Start () {
-    
-        float angle = 2 * Mathf.PI / keys.Count;
-        Vector3 position;
 
-        for(int i = 0; i < keys.Count; i++)
+        Transform[] transforms = GetComponentsInChildren<Transform>();
+        children = GetComponentsInChildren<NoteController>();
+
+        float angle = 2 * Mathf.PI / children.Length;
+        for(int i = 0; i < children.Length; i++)
         {
-            position = new Vector3(ray * Mathf.Cos(angle * i), 1,
-                                   ray * Mathf.Sin(angle * i));
-            BallAttractor attractor = Instantiate(notePrefab, position, Quaternion.identity).GetComponent<BallAttractor>();
-            attractor.SetKey(keys[i]);
+            transforms[i].position = new Vector3(ray * Mathf.Cos(angle * i), 1,
+                                                 ray * Mathf.Sin(angle * i));
         }
 
         time = 0;
@@ -34,7 +31,7 @@ public class NotesController : MonoBehaviour {
     {
         if(time == 0)
         {
-            Fire();
+            FireWave();
         }
         time += Time.deltaTime;
         if (time > timeInterval)
@@ -43,28 +40,27 @@ public class NotesController : MonoBehaviour {
         }
     }
 
-    private void Fire()
+    private void FireWave()
     {
-        int id = Random.Range(0, keys.Count - 1);
+        int id = Random.Range(0, children.Length - 1);
         for(int i = 0; i < id; i++)
         {
-            SendProjectile(i);
+
+            FireProjectile(i);
         }
-        for (int i = id + 1; i < keys.Count; i++)
+        for (int i = id + 1; i < children.Length; i++)
         {
-            SendProjectile(i);
+            FireProjectile(i);
         }
     }
 
-    private void SendProjectile(int note)
+    private void FireProjectile(int i)
     {
-        float angle = 2 * Mathf.PI / keys.Count;
-        Vector3 position = new Vector3(ray * Mathf.Cos(angle * note), 1, ray * Mathf.Sin(angle * note));
-        Quaternion rotation = Quaternion.Euler(90, 0, Mathf.Rad2Deg * angle * note - 90);
-        //Vector3 target = -position + new Vector3(0, 1 + position.y, 0);
-        Vector3 target = new Vector3(0, 1, 0);
-        ProjectileController proj = ProjectileController.Create(projectilePrefab, position, rotation, projectileSpeed, target);
+        float angle = 2 * Mathf.PI * i / children.Length;
+        children[i].SendProjectile(angle, ray, projectileSpeed);
     }
+
+
 
 
 }
